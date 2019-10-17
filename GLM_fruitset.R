9ffb65df6166a10da758e8bbe44625b33fe9b909
Javier Cuadra Valdés <hjcuadra93@gmail.com>
2019-10-17 15:06:19 -0300

    agregar archivos

---
+++ GLM_fruitset.R
@@ -0,0 +1,25 @@
+#GLM for pollinator theft by Euycalyptus
+
+#Load data_fset.txt file
+data<-read.table("./Data/data_fset.txt", header= T)
+attach(data)
+names(data)
+
+library(mgcv)
+library(lme4)
+library(MASS)
+library(AER)
+library(ggplot2)
+
+##Quick Descriptive analysis
+boxplot(fruitset~habitat,data=data, xlab="Habitat", ylab="Fruit set")
+
+#GLM with poisson distribution
+fset<-glm(fruitset~habitat, data=data)
+summary(fset)
+
+qplot(habitat, fruitset, data=data, geom=c("boxplot"), main="",
+   xlab="Habitat", ylab="Promedio de frutos formados")
+
+
+detach(data)
